69e9224f05dbb49087a9e39e93daadd0763e3e75
Learning Helper <learning@innovisionpath.com>
2024-10-30 16:49:29 +0800

    chore: init

---
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace GildedRoseTest
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void Test()
+        {
+            string text = System.IO.File.ReadAllText("sample.approved.txt");
+
+            Assert.Equal("Approved result", text);
+        }
+    }
+}
