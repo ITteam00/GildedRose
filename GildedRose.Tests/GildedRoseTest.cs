f7f41ac004b62fb1c316d992fefa2801019c915d
Xi Wu <XWu35@slb.com>
2024-10-30 19:00:11 +0800

    test: add UpdateQualityTest

--- GildedRose.Tests/GildedRoseTest.cs
+++ GildedRose.Tests/GildedRoseTest.cs
@@ -1,15 +1,44 @@
-﻿using Xunit;
+﻿using GildedRose;
+using Xunit;
 
 namespace GildedRoseTest
 {
     public class GildedRoseTest
     {
         [Fact]
-        public void Test()
+        public void UpdateQualityTest()
         {
-            string text = System.IO.File.ReadAllText("sample.approved.txt");
-
-            Assert.Equal("Approved result", text);
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20,
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49,
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49,
+                },
+            };
+            GildedRose.GildedRose service = new GildedRose.GildedRose(items);
+            service.UpdateQuality();
+            var expectResult = "+5 Dexterity Vest, 9, 19";
+            var actualResult = items[0].Name + ", " + items[0].SellIn + ", " + items[0].Quality;
+            Assert.Equal(actualResult, expectResult);
         }
     }
 }
