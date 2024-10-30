264d13af73c60718cceac64c9da30e18c9356623
yyu26 <yyu26@slb.com>
2024-10-30 20:43:07 +0800

    refactor: test

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -6,5 +6,24 @@ namespace GildedRose
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+
+        protected Item(string name, int sellIn, int quality)
+        {
+            Name = name;
+            SellIn = sellIn;
+            Quality = quality;
+        }
+
+        public virtual void UpdateQuality()
+        {
+            if (Quality < 0)
+            {
+                Quality = 0;
+            }
+            else if (Quality > 50)
+            {
+                Quality = 50;
+            }
+        }
     }
 }
