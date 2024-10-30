ac4579522b651bc0849035b0a7fe96b37b3403b2
kgaohahaha <15296612347@163.com>
2024-10-30 22:31:37 +0800

    refactor: remove loop

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -12,65 +12,10 @@ namespace GildedRose
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < items.Count; i++)
+            foreach (var item in items) 
             {
-                if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    if (items[i].Quality > 0)
-                    {
-                        items[i].Quality--;
-                    }
-                }
-                else
-                {
-                    if (items[i].Quality < 50)
-                    {
-                        items[i].Quality++;
-
-                        if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (items[i].SellIn < 11)
-                            {
-                                    items[i].Quality++;
-                            }
-
-                            if (items[i].SellIn < 6)
-                            {
-                                    items[i].Quality ++;
-                            }
-                        }
-                    }
-                }
-
-                if (items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].SellIn--;
-                }
-
-                if (items[i].SellIn < 0)
-                {
-                    if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (items[i].Quality > 0)
-                        {
-                            if (items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                items[i].Quality = items[i].Quality - 1;
-                            }
-                        }
-                        else
-                        {
-                            items[i].Quality = items[i].Quality - items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (items[i].Quality < 50)
-                        {
-                            items[i].Quality = items[i].Quality + 1;
-                        }
-                    }
-                }
+                var updater=ItemUpdaterSelector.GetUpdater(item);
+                updater.Update(item);
             }
         }
     }
